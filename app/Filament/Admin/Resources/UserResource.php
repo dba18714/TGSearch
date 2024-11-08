e60c5d20bb0f535067396f9450b2deed757f8504
dba18714 <dba18714@gmail.com>
2024-11-09 01:35:49 +0800

    deleted some file and code

--- app/Filament/Admin/Resources/UserResource.php
+++ app/Filament/Admin/Resources/UserResource.php
@@ -4,7 +4,7 @@ namespace App\Filament\Admin\Resources;
 
 use App\Filament\Admin\Resources\UserResource\Pages;
 use App\Filament\Resources\UserResource\RelationManagers;
-use App\Models\Plan;
+
 use Filament\Forms;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
