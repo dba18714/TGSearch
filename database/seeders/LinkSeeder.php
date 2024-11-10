cea9bda424ce6341f917b7cdb9a08c4bcf7b07d0
dba18714 <dba18714@gmail.com>
2024-11-11 00:01:40 +0800

    optimize

--- database/seeders/LinkSeeder.php
+++ database/seeders/LinkSeeder.php
@@ -10,7 +10,7 @@ class LinkSeeder extends Seeder
     public function run(): void
     {
         // 创建 50 个随机类型的 Link
-        Link::factory()->count(50)->create();
+        Link::factory()->count(200)->create();
 
         // 确保每种类型至少有 5 个
         Link::factory()->bot()->count(5)->create();
