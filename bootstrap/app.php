c5cd30f71eadd4fae23b8812d9035c8d7464ccf3
dba18714 <dba18714@gmail.com>
2024-11-23 05:52:09 +0800

    composer require --prefer-dist illuminatech/url-trailing-slash

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,20 +4,26 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-return Application::configure(basePath: dirname(__DIR__))
+$app = Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
         // dd('Middleware loading');
-        $middleware->prepend([
+
+        $middleware->prependToGroup('web', Illuminatech\UrlTrailingSlash\Middleware\RedirectTrailingSlash::class); // enable automatic redirection on incorrect URL trailing slashes
+
+        $middleware->prependToGroup('web', [
             \App\Http\Middleware\RemoveIndexPhp::class,
-            \App\Http\Middleware\NormalizeUrls::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
+
+$app->register(new Illuminatech\UrlTrailingSlash\RoutingServiceProvider($app)); // register trailing slashes routing
+
+return $app;
