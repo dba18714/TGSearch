23e0b28db46cb3babe391b924f35e243645d0ce6
dba18714 <dba18714@gmail.com>
2024-12-11 20:23:17 +0800

    add: laravel pulse

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -45,6 +45,11 @@ class AdminPanelProvider extends PanelProvider
                     ->icon('heroicon-o-presentation-chart-line')
                     ->group('其它')
                     ->sort(3),
+                NavigationItem::make('性能监控(Pulse)')
+                    ->url('/pulse', shouldOpenInNewTab: true)
+                    ->icon('heroicon-o-presentation-chart-line')
+                    ->group('其它')
+                    ->sort(3),
             ])
             ->pages([
                 // Dashboard::class, // 使用自定义的仪表板作为默认页面
