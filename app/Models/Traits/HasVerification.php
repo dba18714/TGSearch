48f00559ba8ecf8ef9d7b8ae52edc1fe365b35c2
dba18714 <dba18714@gmail.com>
2024-12-13 21:06:05 +0800

    scopeSelectForVerification transform to pgsql

--- app/Models/Traits/HasVerification.php
+++ app/Models/Traits/HasVerification.php
@@ -36,10 +36,16 @@ trait HasVerification
 
     public function scopeSelectForVerification($query)
     {
-        return $query
-            ->orderByRaw('ISNULL(verified_start_at) DESC, verified_start_at ASC')
-            ->orderByRaw('ISNULL(verified_at) DESC, verified_at ASC')
+        // pgsql
+        return $query->orderByRaw('verified_start_at ASC NULLS FIRST')
+            ->orderByRaw('verified_at ASC NULLS FIRST')
             ->orderBy('created_at');
+
+        // // mysql
+        // return $query
+        //     ->orderByRaw('ISNULL(verified_start_at) DESC, verified_start_at ASC')
+        //     ->orderByRaw('ISNULL(verified_at) DESC, verified_at ASC')
+        //     ->orderBy('created_at');
     }
 
     protected static function bootHasVerification()
